9b28c3c88839df0a0b86959653f10c855208c914
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-07-12 19:00:53 -0400

    now takes in multiple files

--- hw0/wc.c
+++ hw0/wc.c
@@ -3,49 +3,69 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    printf("hi\n");
+    printf("%s\n", "hi");
 
-    char* fileName = malloc(strlen(argv[0]) + 1); // create space for the string variable
-    strcpy(fileName, argv[1]); // copy the characters over to the fileName
+    int totalNewLineCount = 0;
+    int totalWordCount = 0;
+    int totalCharacterCount = 0;
 
-    FILE *fp; // create a file object
-    fp = fopen(fileName, "r"); // we want to open the file for writing
-    if (fp == NULL) {
-        printf("File does not exist.\n");
-        return 1;
+    int i;
+    for (i = 1; i < argc; i++) {
+
+        char* fileName = malloc(strlen(argv[i]) + 1); // create space for the string variable
+        strcpy(fileName, argv[i]); // copy the characters over to the fileName
+
+        FILE *fp; // create a file object
+        fp = fopen(fileName, "r"); // we want to open the file for writing
+        if (fp == NULL) {
+            printf("%s\n", "File does not exist.");
+            return 1;
+        }
+
+        // initialise all our counters
+        int newLineCount = 0;
+        int wordCount = 0;
+        int characterCount = 0;
+
+        while(1) { // while true (1 in C)
+            char c = fgetc(fp);  // use filegetchar (fgetc) to get the next character
+
+            if (feof(fp)) { // if it's the eof character, break
+                wordCount++;
+                break;
+            }
+
+            if (c == ' ') {
+                wordCount++;
+            }
+
+            if (c == '\n') {
+                newLineCount++;
+                wordCount++;
+            }
+
+            characterCount++;
+        }
+
+        totalNewLineCount += newLineCount;
+        totalWordCount += wordCount;
+        totalCharacterCount += characterCount;
+
+        fclose(fp);
+
+        printf("  %d  ", newLineCount); // prints the number of characters in the file
+        printf(" %d ", wordCount); // prints the number of characters in the file
+        printf("%d ", characterCount); // prints the number of characters in the file
+        printf("%s\n", fileName); // prints the first argument (file name)
+
+        free(fileName); // releases the memory for the fileName
     }
 
-    // initialise all our counters
-    int newLineCount = 0;
-    int wordCount = 0;
-    int characterCount = 0;
+    printf("  %d  ", totalNewLineCount); // prints the total number of characters in the file
+    printf(" %d ", totalWordCount); // prints the total number of characters in the file
+    printf("%d ", totalCharacterCount); // prints the total number of characters in the file
+    printf("%s\n", "total"); // prints the first argument (file name)
 
-    while(1) { // while true (1 in C)
-        char c = fgetc(fp);  // use filegetchar (fgetc) to get the next character
-
-        if (feof(fp)) { // if it's the eof character, break
-            wordCount++;
-            break;
-        }
-
-        if (c == ' ') {
-            wordCount++;
-        }
-
-        if (c == '\n') {
-            newLineCount++;
-            wordCount++;
-        }
-
-        characterCount++;
-    }
-
-    fclose(fp);
-    printf("  %d  ", newLineCount); // prints the number of characters in the file
-    printf(" %d ", wordCount); // prints the number of characters in the file
-    printf("%d ", characterCount); // prints the number of characters in the file
-    printf("%s\n", fileName); // prints the first argument (file name)
-    free(fileName); // releases the memory for the fileName
     return 0;
 }
 
